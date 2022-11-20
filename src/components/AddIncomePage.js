ee4876f17107304ca61f84913c359638d32cfe94
Igor de Castro <igordecastro07@gmail.com>
2022-11-20 19:10:57 -0300

    feat: add transactions pages

---
+++ src/components/AddIncomePage.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export default function AddIncomePage() {
+  return (
+    <MainContainer>
+      <div>
+        <h1>Nova entrada</h1>
+      </div>
+      <form>
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Descrição" />
+        <button>Salvar entrada</button>
+      </form>
+    </MainContainer>
+  );
+}
+
+const MainContainer = styled.div`
+  div:first-child {
+    margin: 24px;
+    width: 88%;
+    height: 11%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    h1 {
+      font-family: "Raleway";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 26px;
+      line-height: 31px;
+      color: #ffffff;
+    }
+  }
+  form {
+    height: 218px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    input {
+      width: 326px;
+      height: 58px;
+    }
+    input::placeholder {
+      font-family: "Raleway";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 23px;
+      color: #000000;
+    }
+    button {
+      width: 326px;
+      height: 46px;
+      background: #a328d6;
+      border-radius: 5px;
+      margin-bottom: 32px;
+      cursor: pointer;
+      font-family: "Raleway";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 20px;
+      line-height: 23px;
+      color: #ffffff;
+    }
+  }
+`;
--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import LoginPage from "./LoginPage";
 import SignUpPage from "./SignUpPage";
 import MainPage from "./MainPage";
 import GlobalStyle from "../assets/GlobalStyle.js"
+import AddExpensePage from "./AddExpensePage";
+import AddIncomePage from "./AddIncomePage";
+
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +14,8 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/home" element={<MainPage />} />
+        <Route path="/add-expense" element={<AddExpensePage />} />
+        <Route path="/add-income" element={<AddIncomePage />} />
       </Routes>
     </BrowserRouter>
   );
